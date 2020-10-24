e5c7419dcc3f7bfc01768c0d5402cff347654328
Edwin van Wijk <edwinvw@gmail.com>
2020-10-24 12:33:20 +0200

    Add email feature

--- src/Services/Ordering/Ordering.API/Application/DomainEventHandlers/OrderStartedEvent/SendEmailToCustomerWhenOrderStartedDomainEventHandler.cs
+++ src/Services/Ordering/Ordering.API/Application/DomainEventHandlers/OrderStartedEvent/SendEmailToCustomerWhenOrderStartedDomainEventHandler.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 using MediatR;
@@ -9,16 +13,42 @@ using Ordering.Domain.Events;
 namespace Ordering.API.Application.DomainEventHandlers.OrderStartedEvent
 {
     public class SendEmailToCustomerWhenOrderStartedDomainEventHandler
-                   //: IAsyncNotificationHandler<OrderStartedDomainEvent>
-    { 
-        public SendEmailToCustomerWhenOrderStartedDomainEventHandler()
+                   : INotificationHandler<OrderStartedDomainEvent>
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly string _daprHttpPort;
+
+        public SendEmailToCustomerWhenOrderStartedDomainEventHandler(IHttpClientFactory clientFactory)
         {
-        
+            this._clientFactory = clientFactory;
+            _daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "50002";
         }
 
-        //public async Task Handle(OrderStartedDomainEvent orderNotification)
-        //{
-        //    //TBD - Send email logic
-        //}
+        public async Task Handle(OrderStartedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            var payload = new
+            {
+                metadata = new
+                {
+                    emailFrom = "eShopOn@dapr.io",
+                    emailTo = notification.UserId,
+                    subject = $"Your eShopOnDapr Order #{notification.Order.Id}",
+                },
+                data = $"Dear {notification.UserName},\n\n" +
+                       $"The status of your order #{notification.Order.Id} " +
+                       $"has changed to {notification.Order.OrderStatus}.\n\n" +
+                       $"Greetings,\n" +
+                       $"The eShopOnDapr team",
+                operation = "create"
+            };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(payload),
+                Encoding.UTF8, "application/json");
+
+            var httpClient = _clientFactory.CreateClient();
+
+            await httpClient.PostAsync($"http://localhost:{_daprHttpPort}/v1.0/bindings/sendmail", content);
+        }
     }
 }
