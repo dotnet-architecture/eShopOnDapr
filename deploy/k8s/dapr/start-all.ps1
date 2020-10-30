a1c742fa3928100a7341f7ccaded949f7a4fd3f8
Edwin van Wijk <edwinvw@gmail.com>
2020-10-30 19:54:49 +0100

    Add services en infra

--- deploy/k8s/dapr/start-all.ps1
+++ deploy/k8s/dapr/start-all.ps1
@@ -1,10 +1,20 @@
 kubectl apply `
     -f ./namespace.yaml `
-    -f ./sqldata.yaml `
-    -f ./nats-streaming.yaml `
+    -f ./secrets.yaml `
     -f ./seq.yaml `
-    -f ./component-pubsub.yaml `
-    -f ./component-sendmail.yaml `
-    -f ./component-zipkin.yaml `
+    -f ./zipkin.yaml `
+    -f ./sqldata.yaml `
+    -f ./redis.yaml `
+    -f ./signalr.yaml `
+    -f ./components/pubsub-redis.yaml `
+    -f ./components/basket-statestore.yaml `
+    -f ./components/sendmail.yaml `
+    -f ./components/zipkin.yaml `
     -f ./catalog.yaml `
-    -f ./ordering.yaml
+    -f ./ordering.yaml `
+    -f ./basket.yaml `
+    -f ./payment.yaml `
+    -f ./backgroundtasks.yaml `
+    -f ./webshoppingagg.yaml `
+    -f ./webspa.yaml `
+    -f ./webstatus.yaml
--- deploy/k8s/dapr/stop-all.ps1
+++ deploy/k8s/dapr/stop-all.ps1
@@ -1,9 +1,18 @@
 kubectl delete `
+    -f ./webspa.yaml `
+    -f ./webstatus.yaml `
+    -f ./webshoppingagg.yaml `
+    -f ./backgroundtasks.yaml `
     -f ./catalog.yaml `
     -f ./ordering.yaml `
-    -f ./component-pubsub.yaml `
-    -f ./component-sendmail.yaml `
-    -f ./component-zipkin.yaml `
+    -f ./basket.yaml `
+    -f ./payment.yaml `
+    -f ./components/pubsub-redis.yaml `
+    -f ./components/basket-statestore.yaml `
+    -f ./components/sendmail.yaml `
+    -f ./components/zipkin.yaml `
+    -f ./signalr.yaml `
     -f ./seq.yaml `
+    -f ./zipkin.yaml `
     -f ./sqldata.yaml `
-    -f ./nats-streaming.yaml
+    -f ./redis.yaml
