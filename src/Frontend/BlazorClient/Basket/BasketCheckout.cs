59d0ad14475c2a9a6cb876d08113476579465131
Sander Molenkamp <a.molenkamp@gmail.com>
2021-07-01 17:38:26 +0200

    WIP

---
+++ src/Frontend/BlazorClient/Basket/BasketCheckout.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eShopOnDapr.BlazorClient.Ordering
+{
+    public class OrderForm
+    {
+        [Required]
+        [Display(Name = "Address")]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Card number")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Cardholder name")]
+        public string CardHolderName { get; set; }
+
+        [Required]
+        [Display(Name = "Expiration date")]
+        public string CardExpirationDate { get; set; }
+
+        [Required]
+        [Display(Name = "Security code")]
+        public string CardSecurityCode { get; set; }
+    }
+}
---
+++ src/Frontend/BlazorClient/UserInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace eShopOnDapr.BlazorClient.Ordering
+{
+    public class UserInfo
+    {
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        private Dictionary<string, string> _claims;
+
+        public UserInfo(AuthenticationStateProvider authenticationStateProvider)
+        {
+            _authenticationStateProvider = authenticationStateProvider
+                ?? throw new ArgumentNullException(nameof(authenticationStateProvider));
+        }
+
+        public string BuyerId => _claims?["sub"];
+
+        public async Task LoadUserInfoAsync()
+        {
+            var authenticationState =
+                await _authenticationStateProvider.GetAuthenticationStateAsync();
+
+            if (authenticationState.User.Identity.IsAuthenticated)
+            {
+                _claims = authenticationState.User.Claims
+                    .ToDictionary(c => c.Type, c => c.Value);
+            }
+        }
+    }
+}
