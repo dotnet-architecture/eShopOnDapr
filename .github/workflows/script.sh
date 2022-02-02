b61b785ce4f725829fbca3313e5a6c10b22c04e3
hugogirard <hugo.girard@microsoft.com>
2022-02-02 10:35:35 -0500

    added scripts back

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,8 +4,8 @@
 apt-get update
 
 # Install Kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-chmod +x kubectl
-mkdir -p ~/.local/bin/kubectl
-mv ./kubectl ~/.local/bin/kubectl
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# chmod +x kubectl
+# mkdir -p ~/.local/bin/kubectl
+# mv ./kubectl ~/.local/bin/kubectl
