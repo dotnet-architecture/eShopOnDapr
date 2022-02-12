3cbafaef4279038e5810ffb849c4f4790f08f428
hugogirard <hugo.girard@microsoft.com>
2022-02-12 09:39:56 -0500

    added node express

---
+++ src/HelloWorlExpress/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+    res.render('<h1>Hello World</h1>');
+});
+
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`);
+});
